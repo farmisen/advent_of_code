c65ea26e5f8ae3134988649a5e247dea9b3e3cae
Fabrice <farmisen@gmail.com>
2022-12-04 22:48:34 -0800

    day 05 part 01 (#10)

---
+++ examples/day_05_part_01/main.rs
@@ -0,0 +1,50 @@
+#![feature(iter_array_chunks)]
+
+use std::{fs, io::Result};
+
+use advent_of_code::{datapath, LINE_END};
+use regex::Regex;
+
+fn main() -> Result<()> {
+    let mut stacks: Vec<Vec<char>> = (1..=9).map(|_| Vec::new()).collect();
+
+    fs::read_to_string(datapath("day_05")?)?
+        .split(LINE_END)
+        .for_each(|line| {
+            if line.contains('[') {
+                line.chars().enumerate().for_each(|(idx, c)| {
+                    if ('A'..='Z').contains(&c) {
+                        stacks[(idx - 1) / 4].push(c);
+                    }
+                });
+            } else if line.starts_with("move") {
+                // println!("[{}]", line);
+                // debug_stacks(&stacks);
+                let re = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
+                let caps = re.captures(line).unwrap();
+                let count = caps
+                    .get(1)
+                    .map_or(0, |m| m.as_str().parse::<u32>().unwrap());
+                let from = caps
+                    .get(2)
+                    .map_or(0, |m| m.as_str().parse::<usize>().unwrap());
+                let to = caps
+                    .get(3)
+                    .map_or(0, |m| m.as_str().parse::<usize>().unwrap());
+
+                (0..count).for_each(|_| {
+                    let t = stacks[from - 1].pop().unwrap();
+                    stacks[to - 1].push(t);
+                });
+            } else if line.is_empty() {
+                (0..=8usize).for_each(|n| stacks[n].reverse());
+            }
+        });
+
+    // HBTMTBSDC
+    print!("Day 05 Part 01:");
+    (0..=8usize).for_each(|n| print!("{}", stacks[n].pop().unwrap()));
+    println!();
+
+    Ok(())
+}
