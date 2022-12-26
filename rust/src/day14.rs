84af25bbd3f9c457e28316a8c935b3007f832970
Fabrice Armisen <farmisen@gmail.com>
2022-12-26 15:25:31 -0800

    rust day 14 (wip)

--- rust/src/day14.rs
+++ rust/src/day14.rs
@@ -7,22 +7,31 @@ pub struct Pos(i64, i64);
 
 impl Pos {
     pub fn path_to(&self, other: &Pos) -> Vec<Self> {
-        let &Pos(x0, y0) = self;
-        let Pos(x1, y1) = other;
+        let &Pos(x_start, y_start) = self;
+        let Pos(x_end, y_end) = other;
 
-        let mut path = vec![];
+        let (dx, dy) = ((x_end - x_start).signum(), (y_end - y_start).signum());
 
-        let (dx, dy) = ((x1 - x0).signum(), (y1 - y0).signum());
-        let mut p = self.clone();
-        loop {
-            p = Pos(p.0 + dx, p.1 + dy);
-            path.push(p.clone());
-            if p.eq(other) {
-                break;
-            }
-        }
+        (x_start..x_end + dx)
+            .step_by(dx)
+            .zip(y_start..y_end + dy)
+            .step_by(dy)
+            .map(|(x, y)| Pos(x, y))
+            .collect()
 
-        path
+        // let mut path = vec![];
+
+        // let (dx, dy) = ((x_end - x_start).signum(), (y_end - y_start).signum());
+        // let mut p = self.clone();
+        // loop {
+        //     p = Pos(p.0 + dx, p.1 + dy);
+        //     path.push(p.clone());
+        //     if p.eq(other) {
+        //         break;
+        //     }
+        // }
+
+        // path
     }
 }
 
@@ -33,34 +42,27 @@ pub enum Unit {
 }
 
 pub fn parse_path(input: &str) -> Vec<Pos> {
-    let t = input
+    input
         .split(" -> ")
         .map(|token| token.split_once(',').unwrap())
         .map(|(str_x, str_y)| Pos(str_x.parse::<i64>().unwrap(), str_y.parse::<i64>().unwrap()))
-        .collect::<Vec<_>>();
+        .fold(Vec::<Pos>::new(), |mut path, pos| {
+            println!("\nPATH:{path:?}, POS:{pos:?}");
 
-    println!("T:{t:?}");
-    let s = t.iter().fold(Vec::<Pos>::new(), |mut path, pos| {
-        println!("\nPATH:{path:?}, POS:{pos:?}");
-
-        let res = match path.iter().last() {
-            None => vec![pos.clone()],
-            Some(p) => {
-                println!("P:{p:?}");
-                let mut pt = p.path_to(pos);
-                println!("PT:{pt:?}");
-                path.append(&mut pt);
-                println!("AP:{path:?}");
-                path
-            }
-        };
-        println!("RES:{res:?}");
-        res
-    });
-
-    println!("\nS:{s:?}");
-
-    s
+            let res = match path.iter().last() {
+                None => vec![pos.clone()],
+                Some(p) => {
+                    println!("P:{p:?}");
+                    let mut pt = p.path_to(&pos);
+                    println!("PT:{pt:?}");
+                    path.append(&mut pt);
+                    println!("AP:{path:?}");
+                    path
+                }
+            };
+            println!("RES:{res:?}");
+            res
+        })
 }
 
 pub fn parse_scan(input: &str) -> HashSet<Unit> {
