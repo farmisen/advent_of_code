dec641b3a40d6e37634e4bb11731d749788b7468
Fabrice Armisen <farmisen@gmail.com>
2023-12-01 00:14:10 -0800

    clean up

--- 2023/kotlin/tests/aockt/y2023/Y2023D01Test.kt
+++ 2023/kotlin/tests/aockt/y2023/Y2023D01Test.kt
@@ -6,15 +6,6 @@ import io.github.jadarma.aockt.test.AdventSpec
 @AdventDay(2023, 1, "Trebuchet?!")
 class Y2023D01Test : AdventSpec<Y2023D01>({
 
-//    test("debug") {
-//        solution.partTwo(  """two1nine
-//            eightwothree
-//            abcone2threexyz
-//            xtwone3four
-//            4nineeightseven2
-//            zoneight234
-//            7pqrstsixteen""")
-//    }
     partOne {
         """1abc2
             pqr3stu8vwx
