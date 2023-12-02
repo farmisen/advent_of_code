9db0c93a6d383c6ddd2ddb18b09082f115752f12
Fabrice Armisen <farmisen@gmail.com>
2023-12-02 00:02:09 -0800

    day 02

---
+++ 2023/kotlin/solutions/aockt/y2023/Y2023D02.kt
@@ -0,0 +1,42 @@
+package aockt.y2023
+
+import io.github.jadarma.aockt.core.Solution
+
+object Y2023D02 : Solution {
+    // Function to parse the input string into a sequence of pairs.
+    // Each pair consists of an integer for the game id and a list of mutable maps for the game sets.
+    private fun parseInput(input: String): Sequence<Pair<Int, List<MutableMap<String, Int>>>> =
+        // Split the input string by newline characters and map each line.
+        input.splitToSequence('\n').map { line ->
+            // Trim each line and split it by the colon character.
+            line.trim().split(":").let { (name, sets) ->
+                // Get the game id and map it to the sets
+                name.split(" ")[1].toInt() to sets.split(";").map { set ->
+                    // For each set (split by semicolons), create a mutable map.
+                    set.trim().split(",").associate {
+                        // Split each entry by space, where the first part is a number
+                        // and the second part is a color. This forms key-value pairs in the map.
+                        val (num, color) = it.trim().split(" ")
+                        color to num.toInt()
+                    }.toMutableMap()
+                }
+            }
+        }
+
+    private val bag = mapOf("red" to 12, "green" to 13, "blue" to 14)
+
+    override fun partOne(input: String) = parseInput(input).sumOf { (id, sets) ->
+        // Return the game id if no set contains more of a color than the same color count in 'bag', otherwise return 0.
+        if (sets.any { set -> set.any { (color, count) -> bag[color]!! < count } }) 0 else id
+    }
+
+    override fun partTwo(input: String) = parseInput(input).sumOf { (_, sets) ->
+        sets.fold(mutableMapOf<String, Int>()) { res, set ->
+            // For each color in the set, update the count in 'res' to be the maximum
+            // of the current count in 'res' and the count in the set.
+            set.forEach { (color, count) -> res[color] = maxOf(res[color] ?: 0, count) }
+            res
+            // Reduce the map values (counts) by multiplying them together.
+        }.values.reduce(Int::times)
+    }
+}
---
+++ 2023/kotlin/tests/aockt/y2023/Y2023D02Test.kt
@@ -0,0 +1,27 @@
+package aockt.y2023
+
+import io.github.jadarma.aockt.test.AdventDay
+import io.github.jadarma.aockt.test.AdventSpec
+
+@AdventDay(2023, 2, "Cube Conundrum ?!")
+class Y2023D02Test : AdventSpec<Y2023D02>({
+
+    val input = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+                    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+                    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+                    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+
+
+    test("debug") {
+        solution.partOne(input )
+    }
+    partOne {
+        input shouldOutput 8
+    }
+
+    partTwo {
+        input shouldOutput 2286
+    }
+
+})
