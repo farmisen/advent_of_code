21a48f788790ea1304467ae585fb522e42c282a5
Fabrice <farmisen@gmail.com>
2023-12-06 23:28:43 -0800

    Day 07 (#14)
     day 07

--- 2023/kotlin/solutions/aockt/y2023/Y2023D06.kt
+++ 2023/kotlin/solutions/aockt/y2023/Y2023D06.kt
@@ -9,8 +9,8 @@ object Y2023D06 : Solution {
     private fun roundUp(n: Double) = if (n % 1.0 != 0.0) ceil(n).toLong() else n.toLong() + 1
     private fun roundDown(n: Double) = if (n % 1.0 != 0.0) floor(n).toLong() else n.toLong() - 1
 
-    private fun ways(a: Double, b: Double) =
-        roundDown(0.5 * (a + sqrt(a * a - 4 * b))) - roundUp(0.5 * (a - sqrt(a * a - 4 * b))) + 1
+    private fun quadraticDiff(a: Double, b: Double) =
+        sqrt(a * a - 4 * b).let { s -> roundDown(0.5 * (a + s)) - roundUp(0.5 * (a - s)) }
 
     private fun parseInput(input: String) = Pair(
         input.substringBefore('\n').substringAfter(':').trim(), input.substringAfter('\n').substringAfter(':').trim()
@@ -19,13 +19,13 @@ object Y2023D06 : Solution {
     override fun partOne(input: String): Long {
         val (times, distances) = parseInput(input)
 
-        return times.split(Regex(" ")).mapNotNull { it.toDoubleOrNull() }
-            .zip(distances.split(Regex(" ")).mapNotNull { it.toDoubleOrNull() }).map { (a, b) ->
-                ways(a, b)
+        return times.split(" ").mapNotNull { it.toDoubleOrNull() }
+            .zip(distances.split(" ").mapNotNull { it.toDoubleOrNull() }).map { (a, b) ->
+                quadraticDiff(a, b) + 1
             }.reduce(Long::times)
     }
 
     override fun partTwo(input: String) =
-        parseInput(input).toList().map { it.replace(" ", "").toDouble() }.let { (a, b) -> ways(a, b) }
+        parseInput(input).toList().map { it.replace(" ", "").toDouble() }.let { (a, b) -> quadraticDiff(a, b) + 1}
 }
 
